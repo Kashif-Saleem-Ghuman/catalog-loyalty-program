3c8e9a0635e09c8e811004133661e502d0cdaba6
kashif <kashif_saleem_23@hotmail.com>
2023-08-09 19:46:54 +0500

    Refac: Add nav bar and the required font to the body

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import Nav from '@/components/Nav'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <body className='font-montserrat'>
+        <Nav />
+        {children}
+
+      </body>
+    </html >
   )
 }
