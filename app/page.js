c7041cd3b23b0d610279c5d69a26ddf54526ead0
kashif <kashif_saleem_23@hotmail.com>
2023-08-17 20:02:05 +0500

    Refac: Add image for the hero section

--- app/page.js
+++ app/page.js
@@ -1,14 +1,16 @@
 import "./globals.css"
 import Image from "next/image"
-import illustration from "@/public/assets/images/Illustration.png"
+import illustration from "@/public/assets/images/hero-desktop.png"
+
 
 export default function Home() {
   return (
     <main className="background pb-20">
-      <section className="xl:flex xl:mx-56 ">
+
+      <section className="xl:flex xl:relative xl:mx-56" style={{ overflow: "visible" }}>
 
 
-        <div className="pt-10 mx-5 px-6 md:mx-auto md:w-5/12 xl:flex xl:flex-col  xl:w-6/12 xl:ps-0 xl:pe-0 xl:mx-0 ">
+        <div className="pt-10 mx-5 px-6 m  d:mx-auto md:w-5/12 xl:flex xl:flex-col  xl:w-6/12 xl:ps-0 xl:pe-0 xl:mx-0 xl:justify-items-end">
           <h2 className="text-center text-Neutral-600 text-base font-semibold tracking-24% xl:text-start xl:text-1-vw">EXPLORE THE</h2>
           <h1 className="mt-2 multicolortext text-center text-mob-25 font-black leading-mob md:text-tab-9 md:leading-tab xl:text-start xl:text-10xl xl:leading-desktop">TECH <span className="text-Neutral-900">ZONE</span></h1>
           <p className="text-center text-Neutral-600 text-base mt-6 font-semibold xl:text-start xl:text-1-vw">Here you’ll be able to redeem all of your hard-earned Aeropoints and exchange them for cool tech.</p>
@@ -28,24 +30,28 @@ export default function Home() {
 
           </article>
         </div>
-        <div className="flex flex-1">
 
-          <Image
+
+        <div className="hidden xl:flex background-color xl:flex-1 xl:mt-10  rounded-[80px] xl:relative z-0"
+          style={{ overflow: "visible" }}
+        >
+          < Image
             src={illustration}
-            alt="Illustration"
-         
-            layout="responsive"
-          
-            style={{ objectFit: "cover" }}
+            alt="hero"
+            layout="fill"
+            style={{ overflow: "visible", maxWidth: "110%", minWidth: "110%" }}
+            className="xl:absolute xl:z-10 xl:bottom-0 xl:left-[-15%]"
+
+
           />
-
         </div>
-
-
 
 
       </section>
 
-    </main>
+
+
+
+    </main >
   )
 }
